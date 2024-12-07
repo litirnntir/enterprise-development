0cdd6fef124a07d5286ce9bb443bced801f35b5c
Alexandra Gorbuntsova <aka-33@yandex.ru>
2024-12-08 00:27:15 +0400

    Added the Supplier class

---
+++ Factory/Factory/Supplier.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Model;
+
+/// <summary>
+/// Class describing supplier
+/// </summary>
+public class Supplier
+{
+    /// <summary>
+    /// Supplier identifier
+    /// </summary>
+    [Key]
+    public int SupplierID { get; set; } = 0;
+
+    /// <summary>
+    /// Supplier's name
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    ///  Address
+    /// </summary>
+    public string Address { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Phone 
+    /// </summary>
+    public string Phone { get; set; } = string.Empty;
+
+    /// <summary>
+    /// List of supplies
+    /// </summary>
+    public List<Supply> Supplies { get; set; } = null!;
+
+    public Supplier() { }
+
+    public Supplier(int supplierID, string name, string address, string phone)
+    {
+        SupplierID = supplierID;
+        Name = name;
+        Address = address;
+        Phone = phone;
+        Supplies = new List<Supply>();
+    }
+}
