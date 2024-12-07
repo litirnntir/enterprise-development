e428450373a84b6a44388fafc6261942b7d6c099
Alexandra Gorbuntsova <aka-33@yandex.ru>
2024-12-08 01:01:24 +0400

    update date format

--- Factory/Factory/Supply.cs
+++ Factory/Factory/Supply.cs
@@ -1,49 +1,52 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
 
-namespace Factory.Model;
-
-/// <summary>
-/// Class describing supplying
-/// </summary>
-public class Supply
+namespace Factory.Model
 {
     /// <summary>
-    /// Supply identifier
+    /// Class describing supplying
     /// </summary>
-    [Key]
-    public int SupplyID { get; set; } = 0;
-
-    /// <summary>
-    /// Enterprise identifier
-    /// </summary>
-    [ForeignKey("Enterprise")]
-    public int EnterpriseID { get; set; } = 0;
-
-    /// <summary>
-    /// Supplier identifier
-    /// </summary>
-    [ForeignKey("Supplier")]
-    public int SupplierID { get; set; } = 0;
-
-    /// <summary>
-    /// Date
-    /// </summary>
-    public DateTime Date { get; set; } = new DateTime(1970, 1, 1);
-
-    /// <summary>
-    /// Goods count
-    /// </summary>
-    public int Quantity { get; set; } = 0;
-
-    public Supply() { }
-
-    public Supply(int supplyID, int enterpriseID, int supplierID, string date, int quantity)
+    public class Supply
     {
-        SupplyID = supplyID;
-        EnterpriseID = enterpriseID;
-        SupplierID = supplierID;
-        Date = DateTime.Parse(date);
-        Quantity = quantity;
+        /// <summary>
+        /// Supply identifier
+        /// </summary>
+        [Key]
+        public int SupplyID { get; set; } = 0;
+
+        /// <summary>
+        /// Enterprise identifier
+        /// </summary>
+        [ForeignKey("Enterprise")]
+        public int EnterpriseID { get; set; } = 0;
+
+        /// <summary>
+        /// Supplier identifier
+        /// </summary>
+        [ForeignKey("Supplier")]
+        public int SupplierID { get; set; } = 0;
+
+        /// <summary>
+        /// Date
+        /// </summary>
+        public DateTime Date { get; set; } = new DateTime(1970, 1, 1);
+
+        /// <summary>
+        /// Goods count
+        /// </summary>
+        public int Quantity { get; set; } = 0;
+
+        public Supply() { }
+
+        public Supply(int supplyID, int enterpriseID, int supplierID, string date, int quantity)
+        {
+            SupplyID = supplyID;
+            EnterpriseID = enterpriseID;
+            SupplierID = supplierID;
+            Date = DateTime.ParseExact(date, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            Quantity = quantity;
+        }
     }
 }
