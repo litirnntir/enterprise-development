c353ce32a08d9df7691244b4085b58eb6944dbe6
Alexandra Gorbuntsova <aka-33@yandex.ru>
2024-12-08 00:25:43 +0400

    Added the OwnershipForm class

---
+++ Factory/Factory/OwnershipForm.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Model;
+
+/// <summary>
+/// Class describing Ownership Form
+/// </summary>
+public class OwnershipForm
+{
+    /// <summary>
+    /// Ownership Form ID
+    /// </summary>
+    [Key]
+    public int OwnershipFormID { get; set; } = 0;
+
+    /// <summary>
+    /// Ownership Form name
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    public OwnershipForm() { }
+
+    public OwnershipForm(int ownershipFormID, string name)
+    {
+        OwnershipFormID = ownershipFormID;
+        Name = name;
+    }
+}
