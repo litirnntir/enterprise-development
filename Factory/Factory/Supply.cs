67dc2f9b047fd3c4f5ce401a84009ed95a4708f6
Alexandra Gorbuntsova <aka-33@yandex.ru>
2024-12-08 00:28:26 +0400

    Added the Supply class

---
+++ Factory/Factory/Supply.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Factory.Model;
+
+/// <summary>
+/// Class describing supplying
+/// </summary>
+public class Supply
+{
+    /// <summary>
+    /// Supply identifier
+    /// </summary>
+    [Key]
+    public int SupplyID { get; set; } = 0;
+
+    /// <summary>
+    /// Enterprise identifier
+    /// </summary>
+    [ForeignKey("Enterprise")]
+    public int EnterpriseID { get; set; } = 0;
+
+    /// <summary>
+    /// Supplier identifier
+    /// </summary>
+    [ForeignKey("Supplier")]
+    public int SupplierID { get; set; } = 0;
+
+    /// <summary>
+    /// Date
+    /// </summary>
+    public DateTime Date { get; set; } = new DateTime(1970, 1, 1);
+
+    /// <summary>
+    /// Goods count
+    /// </summary>
+    public int Quantity { get; set; } = 0;
+
+    public Supply() { }
+
+    public Supply(int supplyID, int enterpriseID, int supplierID, string date, int quantity)
+    {
+        SupplyID = supplyID;
+        EnterpriseID = enterpriseID;
+        SupplierID = supplierID;
+        Date = DateTime.Parse(date);
+        Quantity = quantity;
+    }
+}
